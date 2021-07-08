fdacdd2eb9e14f878eeb97c01fe70eba4f930477
xiongxq <1154680537@qq.com>
2021-07-09 01:42:24 +0800

    first commit

---
+++ src/pages/account/login/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+/**
+ * 登录页
+ */
+class Login extends Component {
+  render() {
+    return (
+      <View>
+        <Text>登录</Text>
+      </View>
+    );
+  }
+}
+
+export default Login;
